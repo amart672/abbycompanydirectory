d256b3beb96d99b2ed12089143d044226312314b
amart672 <amart672@stu.southtexascollege.edu>
2022-09-14 22:47:51 -0500

    added search icon

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <!-- eslint-disable prettier/prettier -->
 <script setup>
   import NavBar from './components/NavBar.vue';
+  import Main from './components/Main.vue';
 </script>
 
 <template>
   <NavBar />
-  <h1>Main Content</h1>
+  <Main />
 </template>
